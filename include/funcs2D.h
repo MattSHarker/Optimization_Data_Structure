86fba4c009b7da4cf5ec6dea5d84bf427f844ca5
mattsharker <mattsharker@gmail.com>
2019-11-24 16:10:33 -0800

    Added a few functions to test algorithms

---
+++ include/funcs2D.h
@@ -0,0 +1,9 @@
+#ifndef __FUNCS_2D__
+#define __FUNCS_2D__
+
+// 2-D functions
+
+template <typename T> T crossInTray(T* aryy);
+template <typename T> T easom(T* arr);
+
+#endif
---
+++ include/funcsND.h
@@ -0,0 +1,10 @@
+#ifndef __FUNCS_ND__
+#define __FUNCS_ND__
+
+// n-D functions
+
+template <typename T> T schwefel(T* arr, int dims);
+template <typename T> T rosenbrock(T* arr, int dims);
+template <typename T> T levy(T* arr, int dims);
+
+#endif
