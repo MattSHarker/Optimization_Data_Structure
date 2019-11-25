38cec1c31040de8d670112a3cd118f0863befe1e
mattsharker <mattsharker@gmail.com>
2019-11-25 14:16:16 -0800

    Creates the results directories if they doesn't exist

--- build.sh
+++ build.sh
@@ -1,8 +1,18 @@
+# exit on failure
 set -e
 
+# create the results directories
+mkdir -p results
+mkdir -p results/data
+mkdir -p results/fitness
+
+# create the build directory and move to it
 mkdir -p build
 cd build
+
+# build the project
 cmake -DCMAKE_BUILD_TYPE=Release ../
 make
+
 
 echo Project has been sucessfully built
