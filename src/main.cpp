622b2f536134537574a1423dfc48e80369069785
mattsharker <mattsharker@gmail.com>
2020-01-02 21:25:47 -0800

    Updated to run optimization algorithms (WIP)

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,32 @@
+
+#include <iostream>
 #include "testDataset.h"
+#include "optimizationAlgorithm.h"
 
 using namespace std;
 
 void run(){ testDatasetClass(); }
 
-int main()
+int main(int argc, char* argv[])
 {
-    run();
+    // run();
+    char* baseDir;
+
+    // check for args
+    if (argc == 0)
+        baseDir = "test";
+    else
+        baseDir = argv[0];
+
+cout << "baseDir: " << baseDir << endl;
+
+    // setup dataset
+    Dataset<double>* data = new Dataset<double>(100, 10);
+
+    runOptimization(data, baseDir);
+
+    // destroy dataset
+    delete data;
+
     return 0;
 }
