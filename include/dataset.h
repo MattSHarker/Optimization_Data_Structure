03a633c1491b55ab73cfe7707312fef493c48e5f
mattsharker <mattsharker@gmail.com>
2020-01-02 21:17:25 -0800

    Changed back to array based matrix, changed variable names, and added functions

--- include/dataset.h
+++ include/dataset.h
@@ -9,32 +9,48 @@ template <class T>
 class Dataset
 {
 private:
-    int popSize;    // # of sets of data (rows)
-    int dimensions; // size of each set  (columns)
+    int rows;    // # of sets of data (rows)
+    int cols; // size of each set  (columns)
 
     int funcCalls;  // number of function calls to perform
     int bestInd;      // the index of the current best fitness
 
     T *fitness;     // the fitness of each set of values (fitness.length == popSize)
 
-public:
     Matrix<T> *data; // holds the numbers to optimize
                      // rows: popSize      cols: dims
-    
+
+    // functions for sorting fitness and data (quick sort)
+    int partitionLowToHigh(int left, int right);
+    int partitionHighToLow(int left, int right);
+    void sortFitnessLowToHigh(int left, int right);
+    void sortFitnessHighToLow(int left, int right);
+
+public:
     // constructors and destructors
     Dataset(int rows, int cols);
     ~Dataset();
 
-    // functions for dimensions and sets
-    int getDimensions();
-    int getPopSize();
+    // functions for cols and sets
+    int getRows();
+    int getCols();
 
     // functions for fitness
     void setFitness(const int position, T value);
     T    getFitness(const int position);
 
-    // operator overlaods
-    Dataset<T> operator = (Dataset const &first);
+    // functions for data
+    void setData(const uint row, const uint col, T value);
+    T    getData(const uint row, const uint col);
+    T*   getDataRow(const uint row);
+
+    // sort population by fitness (sorts rows as well)
+    void sortFitnessLowToHigh();
+    void sortFitnessHighToLow();
+
+    // print functions
+    void printData();
+    void printFitness();
 };
 
 //TODO: Add an assignment operator (=) overload for deep copies
--- src/dataset.cpp
+++ src/dataset.cpp
@@ -1,24 +1,21 @@
 #include <iostream>
-#include <vector>
-
 #include "dataset.h"
 
 using namespace std;
 
-
 template <class T>
-Dataset<T>::Dataset(int newPopSize, int newDims)
+Dataset<T>::Dataset(int newRows, int newCols)
 {
     // set the size variables
-    popSize    = newPopSize;
-    dimensions = newDims;
+    rows = newRows;
+    cols = newCols;
 
     // initialize the matrix
-    data = new Matrix<T>(popSize, dimensions);
+    data = new Matrix<T>(rows, cols);
 
     // initialize fitness array
-    // fitness.length == popSize;
-    fitness = new T[popSize];
+    // fitness.length == rows;
+    fitness = new T[rows];
 }
 
 
@@ -34,16 +31,16 @@ Dataset<T>::~Dataset()
 
 
 template <class T>
-int Dataset<T>::getDimensions()
+int Dataset<T>::getRows()
 {
-    return dimensions;
+    return rows;
 }
 
 
 template <class T>
-int Dataset<T>::getPopSize()
+int Dataset<T>::getCols()
 {
-    return popSize;
+    return cols;
 }
 
 
@@ -51,7 +48,7 @@ template <class T>
 void Dataset<T>::setFitness(int position, T value)
 {
     // check for bounds
-    if (0 > position || position >= popSize)
+    if (0 > position || position >= rows)
         throw out_of_range ("Dataset.setFitness");
 
     // set the value
@@ -63,7 +60,7 @@ template <class T>
 T Dataset<T>::getFitness(int position)
 {
     // check for bounds
-    if (0 > position || position >= popSize)
+    if (0 > position || position >= rows)
         throw out_of_range ("Dataset.getFitness");
 
     // return the value
@@ -71,24 +68,189 @@ T Dataset<T>::getFitness(int position)
 }
 
 template <class T>
-Dataset<T> Dataset<T> operator = (Dataset<T> const &first)
+void Dataset<T>::setData(const uint row, const uint col, T value)
 {
-    Dataset<T> second;
-    second.popSize    = first.popSize;
-    second.dimensions = first.dimensions;
-    second.funcCalls  = first.funcCalls;
-    second.bestInd    = first.bestInd;
+    // check for bounds
+    if (row >= data->getRows())
+        if (row == data->getRows())
+            throw out_of_range ("Dataset.setData: rows (check for off by 1 error");
+        else
+            throw out_of_range ("Dataset.setData: rows");
+    
+    if (col >= data->getCols())
+        if (col == data->getCols())
+            throw out_of_range ("Dataset.setData: cols (check for off by 1 error");
+        else
+            throw out_of_range ("Dataset.setData: cols");
 
-    // copy T* fitness
-    for (int i = 0; i < first.popSize; ++i)
-        second.fitness[i] = first.fitness[i];
+    // set the value
+    data->setValue(row, col, value);
+}
 
-    // copy Matrix<T>
-    for (int i = 0; i < first.popSize; ++i)
-        for (int j = 0; j < first.dimensions; ++j)
-            second.data->setValue(i, j, first.data->getValue(i,j));
+template <class T>
+T Dataset<T>::getData(const uint row, const uint col)
+{
+    // check for bounds
+    if (row > data->getRows())
+        throw out_of_range ("Dataset.setData: Setting a row that is out of range.");
+    
+    if (col > data->getCols())
+        throw out_of_range ("Dataset.setData: Setting a col that is out of range.");
 
-    return second;
+    // return the value
+    return data->getValue(row, col);
+}
+
+template <class T>
+T* Dataset<T>::getDataRow(const uint row)
+{
+    return data->getValueRow(row);
+}
+
+
+template <class T>
+int Dataset<T>::partitionLowToHigh(int left, int right)
+{
+    // the element placed at the right position
+    int pivot = fitness[right];
+
+    // index of smaller element
+    int small = left - 1;
+
+    for (int i = left; i < right; ++i)
+    {
+        // if the current element is smaller than the pivot
+        if (fitness[i] < pivot)
+        {
+            // incriment index of smaller element
+            ++small;
+
+            // swap the fitness at small and i
+            T temp = fitness[small];
+            fitness[small] = fitness[i];
+            fitness[i] = temp;
+
+            // deep swap the entire row at data[small] and data[i] 
+            for (int j = 0; j < cols; ++j)
+            {
+                T temp = data->getValue(small, j);
+                data->setValue(small, j, data->getValue(i, j));
+                data->setValue(i, j, temp);
+            }
+                
+        }
+    }
+
+    T temp = fitness[small+1];
+    fitness[small+1] = fitness[right];
+    fitness[right] = temp;
+
+    for (int i = 0; i < cols; ++i)
+    {
+        T temp = data->getValue(small+1, i);
+        data->setValue(small+1, i, data->getValue(right, i));
+        data->setValue(right, i, temp);
+    }
+
+    return (small + 1);
+}
+
+template <class T>
+int Dataset<T>::partitionHighToLow(int left, int right)
+{
+    // the element placed at the right position
+    int pivot = fitness[right];
+
+    // index of smaller element
+    int small = left - 1;
+
+    for (int i = left; i < right; ++i)
+    {
+        // if the current element is larger than the pivot
+        if (fitness[i] > pivot)
+        {
+            // incriment index of smaller element
+            ++small;
+
+            // swap the fitness at small and i
+            T temp = fitness[small];
+            fitness[small] = fitness[i];
+            fitness[i] = temp;
+
+            // deep swap the entire row at data[small] and data[i] 
+            for (int j = 0; j < cols; ++j)
+            {
+                T temp = data->getValue(small, j);
+                data->setValue(small, j, data->getValue(i, j));
+                data->setValue(i, j, temp);
+            }
+        }
+    }
+
+    T temp = fitness[small+1];
+    fitness[small+1] = fitness[right];
+    fitness[right] = temp;
+
+    for (int i = 0; i < cols; ++i)
+    {
+        T temp = data->getValue(small+1, i);
+        data->setValue(small+1, i, data->getValue(right, i));
+        data->setValue(right, i, temp);
+    }
+
+    return (small + 1);
+}
+
+template <class T>
+void Dataset<T>::sortFitnessLowToHigh()
+{
+    sortFitnessLowToHigh(0, rows-1);
+}
+
+template <class T>
+void Dataset<T>::sortFitnessLowToHigh(int left, int right)
+{
+    if (left < right)
+    {
+        int part = partitionLowToHigh(left, right);
+
+        sortFitnessLowToHigh(left, part-1);
+        sortFitnessLowToHigh(part+1, right);
+    }
+}
+
+template <class T>
+void Dataset<T>::sortFitnessHighToLow()
+{
+    sortFitnessHighToLow(0, rows-1);
+}
+
+template <class T>
+void Dataset<T>::sortFitnessHighToLow(int left, int right)
+{
+    if (left < right)
+    {
+        int part = partitionHighToLow(left, right);
+
+        sortFitnessHighToLow(left, part-1);
+        sortFitnessHighToLow(part+1, right);
+    }
+}
+
+
+template <class T>
+void Dataset<T>::printData()
+{
+    data->print();
+}
+
+template <class T>
+void Dataset<T>::printFitness()
+{
+    for (int i = 0; i < rows; ++i)
+        cout << fitness[i] << " ";
+    
+    cout << endl;
 }
 
 // allow only floats and doubles
