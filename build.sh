bbd307296cfdce0dc8102a93f3e9e12dde08fc2a
mattsharker <mattsharker@gmail.com>
2020-01-05 16:26:23 -0800

    Minor whitespace edits

--- build.sh
+++ build.sh
@@ -1,11 +1,6 @@
 # exit on failure
 set -e
 
-# create the results directories
-mkdir -p results
-mkdir -p results/data
-mkdir -p results/fitness
-
 # create the build directory and move to it
 mkdir -p .build
 cd .build
@@ -13,6 +8,5 @@ cd .build
 # build the project
 cmake -DCMAKE_BUILD_TYPE=Release ../
 make
-
 
 echo Project has been sucessfully built
