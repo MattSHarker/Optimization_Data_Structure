d6010f5f362fc9d1d462ed63d9a8070be7a1aa81
mattsharker <mattsharker@gmail.com>
2020-01-05 22:43:21 -0800

    Completed first version of a python script to analyze the results of the optimization algorithm

--- run.sh
+++ run.sh
@@ -1,9 +1,20 @@
-# create a time for the result's base directory
+# create a name for the result's base directory based off
+    # the time it was ran at
 DATE=$(date +%Y-%m-%d-%H-%M)
-echo "DATE: $DATE"
 
+# remove the directory if it exists to prevent writing to existant files
+rm -rf results/$DATE
+
+# create the data directories
 mkdir -p results/$DATE/data
 mkdir -p results/$DATE/fitness
 mkdir -p results/$DATE/time
 
+# run the optimization
 ./.build/dataset.out $DATE
+
+# create a directory for the analysis
+mkdir -p results/$DATE/analysis
+
+# analyze the data
+python3 ./src/analyze.py $DATE
---
+++ src/analyze.py
@@ -0,0 +1,201 @@
+import csv          # for csv files
+import statistics   # for statistical functions
+import sys          # for argv
+import os
+from decimal import Decimal
+
+# get the current path and move it to the results directory
+os.chdir(os.path.dirname(__file__))
+path = os.getcwd() + "/../results/"
+
+# add the specific subdirectory to the path
+if len(sys.argv) == 1:
+    path += "default"
+else:
+    path += sys.argv[1]
+
+
+# open dataset parameter file to get dimensions
+dims = 0
+with open("../parameters/dataset.txt") as file:
+    dims    = int(file.readline().strip())  # strip() to remove the newline
+    popSize = int(file.readline().strip())
+
+
+# analyze the iteration time file
+# stats: min, max, range, total(sum), mean, median, stdev
+# rows: 1
+
+# create list for data
+iter_stats = [[] for i in range(2)]
+iter_stats[0].extend(["minimum", "maximum", "range", "total", "mean", "median", "stdev"])
+
+# open the data file
+print("Writing iteration_time.csv")
+with open(path + "/time/iteration_time.csv") as csvfile:
+    timeCSV = csv.reader(csvfile, delimiter=',')
+    
+    # read in the iteration times
+    times = []
+    for row in timeCSV:
+        times.append(Decimal(row[1].strip())) # add times to a temp list
+    
+    # analyze the iteration times
+    iter_stats[1].append(min(times))
+    iter_stats[1].append(max(times))
+    iter_stats[1].append(max(times) - min(times))
+    iter_stats[1].append(sum(times))
+    iter_stats[1].append(statistics.mean(times))
+    iter_stats[1].append(statistics.median(times))
+    iter_stats[1].append(statistics.stdev(times))
+
+# create time stats file and write to it
+with open(path + "/analysis/iteration_time_stats.csv", 'w', newline='') as csvfile:
+    filewriter = csv.writer(csvfile, delimiter=',')
+    filewriter.writerows(iter_stats)
+print("iteration_time_stats.csv has been written\n")
+
+
+
+# analyze the fitness file
+# stats: min, max, range, mean, median, stdev
+# rows: iterations
+print("Writing fitness_stats.csv")
+
+
+with open(path + "/fitness/fitness.csv", "r") as csvfile:
+    fitnessCSV = csv.reader(csvfile, delimiter=',')
+
+    # list of lists to hold all information
+    fitness_stats = [[]]
+    fitness_stats[0].extend(["iteration", "minimum", "maximum", "range", "mean", "median", "stdev"])
+
+    # for each row in the file
+    for row in fitnessCSV:
+        # create lists to hold data
+        fitness = []
+        stats = []
+
+        # record the data (skip the first one)
+        for fit in row[1:]:
+            fitness.append(Decimal(fit))
+        
+        # add the stats to the stats list
+        stats.append(row[0])    # iteration
+        stats.append(min(fitness))
+        stats.append(max(fitness))
+        stats.append(max(fitness) - min(fitness))
+        stats.append(statistics.mean(fitness))
+        stats.append(statistics.median(fitness))
+        stats.append(statistics.stdev(fitness))
+
+        # append the stats list to the proper list of lists
+        fitness_stats.append(stats)
+
+# write the fitness stats
+with open(path + "/analysis/fitness_stats.csv", 'w', newline='') as csvfile:
+    filewriter = csv.writer(csvfile, delimiter=',')
+    filewriter.writerows(fitness_stats)
+print("fitness_stats.csv has been written\n")
+
+
+
+# analyze the raw data
+# stdev per column
+# rows: iterations
+# cols: dimensions
+
+print("Writing raw_data_stdev.csv")
+
+# for each file in results/bla/data
+iterations = 30
+# for each file
+
+for i in range(iterations):
+
+    # list to hold standard deviations
+    stdev = []
+
+    #open the file
+    with open(path + "/data/raw_data-" + str(i+1) + ".csv", "r") as csvfile:
+        dataCSV = csv.reader(csvfile, delimiter=',')
+
+        fullData = []
+        # for each row in the file
+        for row in dataCSV:
+            # for each value in the row
+            data = []
+
+            ctr = 0
+            for val in row:
+                data.append(Decimal(val))
+            
+            fullData.append(data)
+
+        # analyze each column (zip transposes cols to rows)
+        for row in zip(*fullData):
+            stdev.append(statistics.stdev(row))
+        
+    # append to raw_data_stdev.csv
+    with open(path + "/analysis/raw_data_stdev.csv", "a+") as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(stdev)
+
+    # analyze and record the stdev data
+    stdev_stats = [min(stdev)]
+    stdev_stats.append(max(stdev))
+    stdev_stats.append(max(stdev) - min(stdev))
+    stdev_stats.append(statistics.mean(stdev))
+    stdev_stats.append(statistics.median(stdev))
+        
+    # append to (or create and write to) stdev_stats.csv
+    with open(path + "/analysis/rd_stdev_stats.csv", "a+") as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(stdev_stats)
+
+print("raw_data_stdev.csv have been written\n")
+
+
+# analyze the raw data standard deviations
+# stats: min, max, range, mean, median, stdev
+# rows: dimensions
+
+# open the new stdev file to analyze it
+print("Writing rd_stdev_stats.csv")
+with open(path + "/analysis/raw_data_stdev.csv", "r") as csvfile:
+    stdevStatsCSV = csv.reader(csvfile, delimiter=',')
+
+    # read the file into lists
+    stdev = []
+    stats = []
+
+    csvfile.readline()  # skip first line
+    for row in stdevStatsCSV:
+        temp = []
+
+        # add each stdev
+        for val in row:
+            temp.append(Decimal(val))
+
+        stdev.append(temp)
+    
+    # analyze each column (transposed into rows)
+    for row in zip(*stdev):
+        temp = []
+        temp.append(min(row))
+        temp.append(max(row))
+        temp.append(max(row) - min(row))
+        temp.append(statistics.mean(row))
+        temp.append(statistics.median(row))
+        temp.append(statistics.stdev(row))
+        
+        # add temp stats list to full stats list 
+        stats.append(temp)
+
+    # create the stdev stats file
+    with open(path + "/analysis/rd_stdev_stats.csv", "w") as csvfile:
+        writer = csv.writer(csvfile, delimiter=',')
+        csvfile.write("minimum,maximum,range,mean,median,stdev\n")
+        writer.writerows(stats)
+
+print("rd_stdev_stats.csv has been written")
