f57ab6a226899c9cd7fe7a1d7ffcf2351772de5c
mattsharker <mattsharker@gmail.com>
2020-01-17 16:02:09 -0800

    Minor formatting changes and comment updates

--- src/timer.cpp
+++ src/timer.cpp
@@ -6,47 +6,49 @@
 
 using namespace std;
 
+// timer defaults to activated
 Timer::Timer()
 {
     active = true;
 }
 
+// grab the current time
 void Timer::start()
 {
-    if (active)
-        time = clock();
+    if (active) time = clock();
 }
 
+// calculate difference btwn start and stop times
 void Timer::stop()
 {
-    if (active)
-        time = clock() - time;
+    if (active) time = clock() - time;
 }
 
 // returns the elapsed time in milliseconds
 double Timer::getTimeMS()
 {
-    if (active)
-        return double(time*1000)/CLOCKS_PER_SEC;
+    if (active) return double(time*1000)/CLOCKS_PER_SEC;
 }
 
 // returns the elapsed time in clock ticks
 clock_t Timer::getTimeTicks()
 {
-    if (active)
-        return time;
+    if (active) return time;
 }
 
+// activates the timer
 void Timer::activate()
 {
     active = true;
 }
 
+// deactivates the timer
 void Timer::deactivate()
 {
     active = false;
 }
 
+// returns the activation state
 bool Timer::activated()
 {
     return active;
