1730d4ad11bd2d20d477260fb68c6409a4c3fa89
mattsharker <mattsharker@gmail.com>
2020-02-11 20:55:37 -0800

    Renamed variables and functions

--- c/include/ods.h
+++ c/include/ods.h
@@ -5,14 +5,14 @@
 
 typedef struct ods
 {
-    unsigned int popSize;    // rows
-    unsigned int dims;       // cols
+    unsigned int popSize;   // rows
+    unsigned int dims;      // cols
 
-    unsigned int funcCalls;
+    unsigned int funcCalls; // init to 0 (per iteration, not total)
 
-    Matrix* data;
+    Matrix* data;           // rows * cols
 
-    double* fitness;    // size: popSize (rows)
+    double* fitness;        // size: popSize (rows)
 
 } ODS;
 
@@ -23,14 +23,12 @@ void destructODS(ODS* ods);
 void   setODSValue(ODS* ods, unsigned int row, unsigned int col, double val);
 double getODSValue(ODS* ods, unsigned int row, unsigned int col);
 
-double* getODSRow(ODS* ods, unsigned int row);
-
 // not necessary, get rid of?
 void incrementFuncCalls(ODS* ods);
 
-// also sort fitness accordingly
-void sortODSLowFirst (ODS* ods);
-void sortODSHighFirst(ODS* ods);
+// also sorts fitness accordingly
+void sortODSLowToHigh(ODS* ods);    // lowest at index 0
+void sortODSHighToLow(ODS* ods);    // highest at index 0
 
 // calculates the fitness of the full
 void calculateAllFitness(ODS* ods);
