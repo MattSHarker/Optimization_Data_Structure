760cd582448c5d921d060b8e01c5b376cd1385e5
mattsharker <mattsharker@gmail.com>
2020-02-11 20:56:27 -0800

    Updated bFO to update ods->funcCalls

--- c/src/baseFunction.c
+++ c/src/baseFunction.c
@@ -13,7 +13,11 @@ void baseFunctionFull(ODS* ods)
 
 void baseFunctionOnce(ODS* ods, unsigned int row)
 {
+    // assign the fitness to the fitness array
     ods->fitness[row] = schwefel(ods, row);
+
+    // incriment the number of function calls
+    ++ods->funcCalls;
 }
 
 double schwefel(ODS* ods, unsigned int row)
